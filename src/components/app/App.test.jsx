0daadf9e4d5d3539ebd67d8cccc3cabb796d1ff7
Elise Muellerleile <eliamue@live.com>
2021-09-15 17:21:07 -0500

    App test built out more

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -5,6 +5,14 @@ import { setupServer } from 'msw/node';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
+const server = setupServer(
+  rest.get('https://asdfjkhasdkfjhasdkjlfhasd.asdfasd', (req, res, ctx) => {
+    return res(
+      ctx.json({ data: { Thing: [{ param: 'thing', text: 'Boopboop' }] } })
+    );
+  })
+);
+
 describe('App', () => {
   it('renders list of thing', async () => {
     render(
@@ -16,7 +24,9 @@ describe('App', () => {
     screen.getByAltText('Loading spinner');
 
     const ul = await screen.findByRole('list', { name: 'thing' });
-    screen.debug();
     expect(ul).not.toBeEmptyDOMElement();
+
+    const li = screen.getByText('boopboop');
+    expect(li).toBeInTheDocument();
   });
 });
