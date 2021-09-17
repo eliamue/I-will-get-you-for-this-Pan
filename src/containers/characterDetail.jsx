49e974c9087a31aff87056daeada9be435d094c1
Elise Muellerleile <eliamue@live.com>
2021-09-16 21:07:51 -0500

    characterDetail added

--- src/containers/characterDetail.jsx
+++ src/containers/characterDetail.jsx
@@ -1,7 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useParams, Link } from 'react-router';
+import Detail from '../components/characters/Detail';
+import { oneCharacterUtility } from '../hooks/oneCharacterUtility';
 
-export default class characterDetail extends Component {
-  render() {
-    return <div></div>;
-  }
-}
+const spinner =
+  'https://c.tenor.com/yalDO5GyNA8AAAAC/screaming-sun-rick-and-morty.gif';
+
+const characterDetail = () => {
+  const { id } = useParams();
+  const { loading, character } = oneCharacterUtility(id);
+
+  if (loading) return <img src={spinner} alt="Loading" />;
+
+  return (
+    <>
+      <Link to="/">
+        <h2>Back to list</h2>
+      </Link>
+      <Detail {...character} />
+    </>
+  );
+};
+
+export default characterDetail;
