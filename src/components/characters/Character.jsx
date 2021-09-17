c110ec83dde237436e2ce3e74a924e41bc549ed3
Elise Muellerleile <eliamue@live.com>
2021-09-16 20:46:58 -0500

    character added

---
+++ src/components/characters/Character.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from '../../styles/Character.css';
+
+const Character = ({ name, image }) => (
+  <figure className={styles.character}>
+    <img className={styles.charImage} src={image} alt={name} />
+    <figcaption>
+      <h2 className={styles.charName}>{name}</h2>
+    </figcaption>
+  </figure>
+);
+
+Character.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+};
+
+export default Character;
